cb69010f7d8d7fe2aea3384bc16946c79751e083
Felipe Besson <flpbesson@gmail.com>
2018-06-24 09:29:07 +0200

    adds indexer

---
+++ saiyan/indexer.py
@@ -0,0 +1,54 @@
+import json
+import elasticsearch.helpers
+from elasticsearch import Elasticsearch
+
+class Indexer:
+
+    def __init__(self, es):
+        self.__index = 'saiyan'
+        self.__es = es
+
+    def index(self):
+        self.__es.indices.delete(self.__index, ignore=[400, 404])
+
+        def bulk_place_docs():
+            with open('dataset/business.json') as data:
+                for line in data:
+                        _doc = json.loads(line)
+                        _doc['reviews'] = []
+
+                        doc = {'_index': self.__index,
+                               '_type': 'place',
+                               '_id': _doc['business_id'],
+                               '_source': _doc}
+
+                        print(doc)
+                        yield doc
+
+        print('------------------------- Adding reviews -----------------------------')
+
+        def bulk_review_docs():
+            with open('dataset/review.json') as data:
+                for line in data:
+                    _doc = json.loads(line)
+
+                    doc = {'_index': self.__index,
+                           '_id': _doc['business_id'],
+                           '_op_type': 'update',
+                           '_type': 'place',
+                           'script' : {
+                                'source': 'ctx._source.reviews.add(params.review)',
+                                'lang' : 'painless',
+                                'params' : {'review' : _doc}
+                                }
+                            }
+
+                    print(doc)
+                    yield doc
+
+        elasticsearch.helpers.bulk(es, bulk_place_docs())
+        elasticsearch.helpers.bulk(es, bulk_review_docs())
+
+if __name__ == '__main__':
+    es = Elasticsearch([{'host': 'localhost',  'port': 9200}])
+    Indexer(es).index()
