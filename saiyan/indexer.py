131741a4ccee971f8c404065223733cdbb8dd696
Felipe Besson <flpbesson@gmail.com>
2018-07-11 23:48:33 +0200

    improves indexer

--- saiyan/indexer.py
+++ saiyan/indexer.py
@@ -1,6 +1,8 @@
 import json
 import elasticsearch.helpers
 from elasticsearch import Elasticsearch
+import sys
+
 
 class Indexer:
 
@@ -8,15 +10,14 @@ class Indexer:
         self.__index = 'saiyan'
         self.__es = es
 
-    def index(self):
-        self.__es.indices.delete(self.__index, ignore=[400, 404])
-        self.__es.indices.create(self.__index, body=json.load(open('elasticsearch/mappings.json')))
+    def index(self, mode = 'all'):
 
         def bulk_place_docs():
             with open('dataset/business.json') as data:
                 for line in data:
                         _doc = json.loads(line)
                         _doc['reviews'] = []
+                        _doc['photos'] = []
 
                         doc = {'_index': self.__index,
                                '_type': 'place',
@@ -37,19 +38,51 @@ class Indexer:
                            '_id': _doc['business_id'],
                            '_op_type': 'update',
                            '_type': 'place',
-                           'script' : {
+                           'script': {
                                 'source': 'ctx._source.reviews.add(params.review)',
-                                'lang' : 'painless',
-                                'params' : {'review' : _doc}
+                                'lang': 'painless',
+                                'params': {'review': _doc}
                                 }
                             }
 
                     print(doc)
                     yield doc
 
-        elasticsearch.helpers.bulk(es, bulk_place_docs())
-        elasticsearch.helpers.bulk(es, bulk_review_docs())
+        def bulk_photo_docs():
+            with open('dataset/photos.json') as data:
+                for line in data:
+                    _doc = json.loads(line)
+
+                    doc = {'_index': self.__index,
+                           '_id': _doc['business_id'],
+                           '_op_type': 'update',
+                           '_type': 'place',
+                           'script': {
+                               'source': 'ctx._source.photos.add(params.photo)',
+                               'lang': 'painless',
+                               'params': {'photo': _doc}
+                           }
+                           }
+
+                    print(doc)
+                    yield doc
+
+        if mode == 'all':
+            self.__es.indices.delete(self.__index, ignore=[400, 404])
+            self.__es.indices.create(self.__index, body=json.load(open('elasticsearch/mappings.json')))
+
+            elasticsearch.helpers.bulk(es, bulk_place_docs())
+            elasticsearch.helpers.bulk(es, bulk_review_docs())
+            elasticsearch.helpers.bulk(es, bulk_photo_docs())
+        elif mode == 'place':
+            elasticsearch.helpers.bulk(es, bulk_place_docs())
+        elif mode == 'review':
+            elasticsearch.helpers.bulk(es, bulk_review_docs())
+        elif mode == 'photo':
+            elasticsearch.helpers.bulk(es, bulk_photo_docs())
+
 
 if __name__ == '__main__':
     es = Elasticsearch([{'host': 'localhost',  'port': 9200}])
-    Indexer(es).index()
+    mode = sys.argv[1] if len(sys.argv) > 1 else 'all'
+    Indexer(es).index(mode)
